ec1de9f2046fa85f4b9dff71661e6b24d471072f
brenocwb02 <breno.albuquerque@gmail.com>
2025-09-27 00:33:04 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -91,18 +91,8 @@ const Index = () => {
 
   return (
     <div className="min-h-screen bg-background flex">
-      {/* Sidebar */}
-      <div className="hidden lg:block">
-        <Sidebar />
-      </div>
-      
-      {/* Mobile Sidebar Overlay */}
-      <div className="lg:hidden">
-        <Sidebar />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col">
+      <Sidebar />
+      <div className="flex-1 flex flex-col sm:pl-14">
         <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
         
         <main className="flex-1 p-6 space-y-6 animate-fade-in">
